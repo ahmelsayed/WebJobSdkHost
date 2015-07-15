830ab1ac8a5bbf9cd0c721ff228dcac76e682526
Ahmed ElSayed <ahmels@microsoft.com>
2015-07-15 12:53:25 -0700

    Initial commit

---
+++ DynamicAssemblyTypeLocator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace WebJobSdkHost
+{
+    class DynamicAssemblyTypeLocator : ITypeLocator
+    {
+        private Assembly _assembly;
+
+        public DynamicAssemblyTypeLocator(Assembly assembly)
+        {
+            this._assembly = assembly;
+        }
+        public IReadOnlyList<Type> GetTypes()
+        {
+            return this._assembly.GetTypes();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.WindowsAzure.Storage;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace WebJobSdkHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var syntax = Directory
+                .GetFiles(Environment.CurrentDirectory, "*.cs")
+                .Select(File.ReadAllText)
+                .Select(c => CSharpSyntaxTree.ParseText(c));
+
+            var refs = new[]
+            {
+                typeof(object),
+                typeof(Enumerable),
+                typeof(JobHost),
+                typeof(QueueAttribute),
+                typeof(CloudStorageAccount),
+                typeof(JToken)
+            }
+            .Select(s => MetadataReference.CreateFromFile(s.Assembly.Location));
+
+            var compilation = CSharpCompilation.Create(Path.GetRandomFileName(), syntax, refs, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+                if (!result.Success)
+                {
+                    var failures = result.Diagnostics.Where(d => d.IsWarningAsError || d.Severity == DiagnosticSeverity.Error);
+                    failures.ToList().ForEach(e => Console.WriteLine("{0}: {1}", e.Id, e.GetMessage()));
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    var assembly = Assembly.Load(ms.ToArray());
+                    var config = new JobHostConfiguration
+                    {
+                        TypeLocator = new DynamicAssemblyTypeLocator(assembly),
+                    };
+                    var host = new JobHost(config);
+                    host.RunAndBlock();
+                }
+            }
+        }
+    }
+}
